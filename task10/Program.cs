7140f3dca78464c5a4580207da4f842c002d8a46
Nataly <natalya-kotenkova@yandex.ru>
2022-11-27 17:06:20 +0100

    added task10

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write ("Ведите трехзначное число :");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n%100;
+int n2 = n%10;
+int n3  = ((n1 - n2)/10);
+Console.Write(n3);
+
