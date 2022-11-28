1ef0df6d48b44d27528487541e3e4360a750919f
Nataly <natalya-kotenkova@yandex.ru>
2022-11-28 10:55:30 +0100

    added task13

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-Console.Write ("Ведите трехзначное число :");
+Console.Write ("Введите трехзначное число :");
 int n = Convert.ToInt32(Console.ReadLine());
 int n1 = n%100;
 int n2 = n%10;
---
+++ task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write ("Введите цифру дня недели :");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n > 1||n < 7)
+
+          if (n > 6)
+          Console.Write (" yes ");
+         
+          else
+          Console.Write (" no" );
+
+          
+
+
+
