1e6ffe1062fb870100e98e63bc0b4ab8e717c33c
Nataly <natalya-kotenkova@yandex.ru>
2022-11-28 11:31:36 +0100

    added task15

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,13 +1,14 @@
 ﻿Console.Clear();
-Console.Write ("Введите цифру дня недели :");
+Console.Write ("Введите цифру дня недели : ");
 int n = Convert.ToInt32(Console.ReadLine());
-while (n > 1||n < 7)
-
-          if (n > 6)
-          Console.Write (" yes ");
-         
-          else
-          Console.Write (" no" );
+ 
+if (n > 7)
+Console.Write ("Вы ошиблись! Введите цифру дня недели от 1 до 7: ");
+else 
+if (n > 5 )
+Console.Write (" yes ");
+else
+Console.Write (" no" );
 
           
 
